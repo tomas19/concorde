7cbcf6e507737de10f23d622e7c2ba3ff0138711
Tomas Cuevas <tomascuevas@gmail.com>
2022-05-20 09:45:18 -0400

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,74 @@
+import os
+
+def animate_list_imgs(imgs, fps, anim_name):
+    ''' Function to animate list of images.
+        Parameters
+            list_imgs: list
+                list with complete path of all imgs
+            fps: int
+                frames per second
+            anim_name: str
+                output name, only name without path. The video will be saved in the notebook path. For the moment, only avi extension has been tested
+        Returns
+            None
+    '''
+    im = cv2.imread(imgs[0])
+    h, w, l = im.shape
+    size = (w, h)
+    
+    out = cv2.VideoWriter(anim_name, cv2.VideoWriter_fourcc(*'DIVX'), 10, size)
+    
+    for i in tqdm(imgs):
+        im = cv2.imread(i)
+        out.write(im)
+    out.release()
+
+def get_list(directory, ends = None, starts = None, contains = None, not_contains = None):
+    '''Generates list with all the files path (path + file name) inside directory which
+        starts with "starts" and ends with "ends"
+
+    Parameters:
+        directory: string
+            Folder path
+        ends: string. Optional, default None
+            e.g. the file extension, '.csv', '.txt', etc
+        starts: string. Optional, default None
+            e.g. the proyect number 'A2022'
+        contains= string. Optional, default None
+            e.g. 'time series' in the middle of the name
+    ---------------------------------------------------------------------------------------
+    Returns:
+        List with paths of the files which satisfy the conditions
+    '''
+    listfiles = []
+    if (ends == None and starts == None):
+        for cur, dirs, files in os.walk(directory): #genera lista con la direccion de cada archivo gz dentro de la carpeta NCfiles
+            for f in files:
+                listfiles.append(os.path.join(cur, f))
+    elif (ends != None and starts == None):
+        for cur, dirs, files in os.walk(directory): #genera lista con la direccion de cada archivo gz dentro de la carpeta NCfiles
+            for f in files:
+                if f.endswith(ends):
+                    listfiles.append(os.path.join(cur, f))
+    elif (ends == None and starts != None):
+        for cur, dirs, files in os.walk(directory): #genera lista con la direccion de cada archivo gz dentro de la carpeta NCfiles
+            for f in files:
+                if f.startswith(starts):
+                    listfiles.append(os.path.join(cur, f))
+    else:
+        for cur, dirs, files in os.walk(directory): #genera lista con la direccion de cada archivo gz dentro de la carpeta NCfiles
+            for f in files:
+                if (f.endswith(ends) and f.startswith(starts)):
+                    listfiles.append(os.path.join(cur, f))
+    if contains != None:
+        listfiles = [x for x in listfiles if contains in x]
+    if not_contains != None:
+        listfiles = [x for x in listfiles if not_contains not in x]
+        
+    if len(listfiles) == 0:
+        sys.exit('The list is empty')
+        return None
+    else:
+        return listfiles
+
+    
